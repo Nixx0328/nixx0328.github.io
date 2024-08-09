b5b87a1eb68c71d77b756d54d05a19ed8c297cc8
Programmer_Ni <Ni502703@aliyun.com>
2024-08-09 19:06:11 +0800

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -1,4 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>我的第一个PHP页面</h1>
+
 <?php
-echo("Hello visitor!Here are codingni.net 's PHP Infomation<br>");
+echo("Hello visitor!Here are codingni.net 's PHP Infomation<br>);
+echo("<a herf="index.html" target="_self">返回CodingNi</a>);
 phpinfo();
 ?>
+
+</body>
+</html>
